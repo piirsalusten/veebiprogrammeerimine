26020014a105aed24bec66b4a807779e054d0637
Sten Piirsalu <piirsalusten@users.noreply.github.com>
2017-09-14 10:47:05 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sten Piirsalu programmeerib veebi</title>
+</head>
+<body>
+	<h1>Sten Piirsalu, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Kodunetöö tehtud</p>
+	<?php
+		echo "Algas PHP õppimine."; 
+	
+	?>
+
+</body>
+</html>
